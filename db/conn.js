07962e3c082441f1bf09f64c0c33a151dcff58de
Pradum Sonkar <pradumsonkar9112@gmail.com>
2023-07-25 12:31:36 +0530

    env

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DB ="mongodb+srv://pradumsonkar911252:pradum911252@cluster0.tm4ctcy.mongodb.net/mernstack?retryWrites=true&w=majority"
+const DB = process.env.DATABASE;
 
 mongoose.connect(DB,{
     useNewUrlParser:true,
